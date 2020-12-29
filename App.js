3d7bd11fefea294f90402e661784edb69d1c014a
alestor123 <alestoraldous@gmail.com>
2020-12-29 15:38:10 +0530

    added  logger

--- App.js
+++ App.js
@@ -10,3 +10,20 @@ options  = require('minimist')(process.argv.slice(2)),
 axios = require('axios'),
 api = 'https://api.github.com/graphql',
 token = process.env.TOKEN || options.token || options.t;
+
+
+
+
+
+
+
+
+
+
+// logger 
+function logger(message){
+    console.log(chalk.bgYellow.red(`(LOG):${Date()}:${message}`))
+}    
+logger.req = (message,req) => {
+    console.log(chalk.bgYellow.blue(`(REQUEST):${Date()}:Ip : ${req.ip} : ${message}`))
+}
