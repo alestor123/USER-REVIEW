05cf7732e2df810322317e0b7f3380c43358b078
alestor123 <alestoraldous@gmail.com>
2020-12-29 15:35:43 +0530

    added api

--- App.js
+++ App.js
@@ -1,3 +1,10 @@
 #!/usr/bin/env node
 
+// setting dotenv
 require('dotenv').config()
+
+var express = require('express'),
+app = express(),
+chalk = require('chalk'),
+axios = require('axios'),
+api = 'https://api.github.com/graphql';
