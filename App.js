c698b59a8ef6f6072d904c04528abc8b0807edc4
alestor123 <alestoraldous@gmail.com>
2020-12-29 15:37:00 +0530

    added token

--- App.js
+++ App.js
@@ -6,5 +6,7 @@ require('dotenv').config()
 var express = require('express'),
 app = express(),
 chalk = require('chalk'),
+options  = require('minimist')(process.argv.slice(2)),
 axios = require('axios'),
-api = 'https://api.github.com/graphql';
+api = 'https://api.github.com/graphql',
+token = process.env.TOKEN || options.token || options.t;
